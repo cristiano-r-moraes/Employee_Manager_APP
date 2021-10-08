14e4511eca7ac50f5bea4a9493bf7cf38f400108
Cristiano Rausse <cristiano.rausse@gmail.com>
2021-10-08 19:29:40 -0300

    api setup methods

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
+
+class employees(Resource):
+    def get(self):
+        return
+    
+    def put(self):
+        return
+    
+    def delete(self):
+        return
+
+api.add_resource(employees, "")
 
 if __name__ == "__main__":
     app.run(debug=True)
