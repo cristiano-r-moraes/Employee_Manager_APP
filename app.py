9c864b96c6010942e23fc4826b851e77c88384b1
Cristiano Rausse <cristiano.rausse@gmail.com>
2021-10-18 10:50:00 -0300

    comments updated

--- app.py
+++ app.py
@@ -27,15 +27,13 @@ class EmployeeModel(db.Model):
     def __repr__(self):
         return f"Employee(name = {self.name}, email = {self.email}, department = {self.department}, salary = {self.salary}, birth_date = {self.birth_date})"
 
-db.create_all()
-
+#This part is referring to -> passing or updating the attributes for each object
 employee_put_args = reqparse.RequestParser()
 employee_put_args.add_argument("name", type=str, help="Please fill in your name.", required=True)
 employee_put_args.add_argument("email", type=str, help="Please fill in your email address.", required=True)
 employee_put_args.add_argument("department", type=str, help="Please fill in the name of department.", required=True)
 employee_put_args.add_argument("salary", type=float, help="Please fill in the salary.", required=True)
 employee_put_args.add_argument("birth_date", type=str, help="Please fill the birth_date.", required=True)
-#employee_put_args.add_argument("birth_date", type=lambda x: datetime.strptime(x, '%d-%m-%Y'))
 
 employee_update_args = reqparse.RequestParser()
 employee_update_args.add_argument("name", type=str, help="Please fill in your name.")
@@ -43,17 +41,16 @@ employee_update_args.add_argument("email", type=str, help="Please fill in your e
 employee_update_args.add_argument("department", type=str, help="Please fill in the name of department.")
 employee_update_args.add_argument("salary", type=float, help="Please fill in the salary.")
 employee_put_args.add_argument("birth_date", type=str, help="Please fill the birth_date.")
-#employee_put_args.add_argument("birth_date", type=lambda x: datetime.strptime(x, '%d-%m-%Y'))
 
 
-#Get the values from the environment variables s
+#Get the values from the environment variables 
 Admin_key = os.environ['admin_key']
 Password_key = os.environ['password']
 
 USER_DATA = {
     Admin_key : Password_key     
 }
-
+#Function to validate the authentication attributes
 @auth.verify_password
 def verify(username, password):
     if not (username and password):
@@ -61,6 +58,7 @@ def verify(username, password):
     return USER_DATA.get(username) == password
 
 
+#Resource referring to the Eployees
 class Employees(Resource):
     #Resource Fields -> Employees
     employee_fields = {
@@ -70,9 +68,8 @@ class Employees(Resource):
                         'department': fields.String,
                         'salary': fields.Float,
                         'birth_date': fields.String
-                        #'birth_date': fields.DateTime(dt_format='rfc822')
                       }
-
+    #Implementation of the GET method 
     @auth.login_required #authentication required -> added in all methods
     @marshal_with(employee_fields) #this decorator is meant to make the EmployeesModel object serializable
     def get(self, employee_id=None):
@@ -86,7 +83,8 @@ class Employees(Resource):
             if not result:
                 abort(404, message="Could not find employee with this id")
             return result
-        
+
+    #Implementation of the PUT method    
     @auth.login_required    
     @marshal_with(employee_fields)
     def put(self, employee_id): 
@@ -100,6 +98,7 @@ class Employees(Resource):
         db.session.commit()
         return employee, 201 #201 - added the employee successfully
 
+    #Implementation of the PATCH method 
     @auth.login_required
     @marshal_with(employee_fields)
     def patch(self, employee_id):
@@ -116,13 +115,14 @@ class Employees(Resource):
             result.department = args['department']
         if args['salary']:
             result.salary = args['salary']
-        #if args['birth_date']:
-        #    result.birth_date = args['birth_date']
+        if args['birth_date']:
+            result.birth_date = args['birth_date']
         
         db.session.commit()
 
         return result
     
+    #Implementation of the DELETE method 
     @auth.login_required
     @marshal_with(employee_fields)
     def delete(self, employee_id):
@@ -133,49 +133,58 @@ class Employees(Resource):
         db.session.commit()
         return '', 204 #204 - deleted successfully        
 
+
+#Resource referring to the Salary Reports
 class Reports_salary(Resource):
     #Resource Fields -> Salary report
-    reports_salary_fields = {}
-    reports_salary_fields['id'] = fields.Integer(attribute='id')
-    reports_salary_fields['name'] = fields.String(attribute='name')
-    reports_salary_fields['email'] = fields.String(attribute='email')
-    reports_salary_fields['department'] = fields.String(attribute='department')
-    reports_salary_fields['salary'] = fields.Float(attribute='salary')
+    resource_fields = {}
+    resource_fields['id'] = fields.Integer(attribute='id')
+    resource_fields['name'] = fields.String(attribute='name')
+    resource_fields['email'] = fields.String(attribute='email')
+    resource_fields['department'] = fields.String(attribute='department')
+    resource_fields['salary'] = fields.Float(attribute='salary')
+    resource_fields['birth_date'] = fields.String(attribute='birth_date')
 
-    resource_fields = {}    
-    resource_fields['lowest'] = fields.Nested(reports_salary_fields)
-    resource_fields['highest'] = fields.Nested(reports_salary_fields)
-    resource_fields['average'] = fields.Float
-                        
+    reports_salary_fields = {}    
+    reports_salary_fields['lowest'] = fields.Nested(resource_fields)
+    reports_salary_fields['highest'] = fields.Nested(resource_fields)
+    reports_salary_fields['average'] = fields.Float
+
+    #Implementation of the GET method                     
     @auth.login_required
-    @marshal_with(resource_fields)
+    @marshal_with(reports_salary_fields)
     def get(self):                              
+            #This section is to get the data referring to the salary 
             max_salary = db.session.query(db.func.max(EmployeeModel.salary)).scalar()
-            result_max = EmployeeModel.query.filter_by(salary=max_salary).first() #get the line correspondent -> serialize this object
+            result_max = EmployeeModel.query.filter_by(salary=max_salary).first() 
             
             min_salary = db.session.query(db.func.min(EmployeeModel.salary)).scalar()
             result_min = EmployeeModel.query.filter_by(salary=min_salary).first()
 
-            avg_salary = db.session.query(db.func.avg(EmployeeModel.salary)).scalar()         
-         
+            avg_salary = db.session.query(db.func.avg(EmployeeModel.salary)).scalar()
+
+            #Model to output the information to the user
             employee_1 = {'id': result_min.id,
                           'name': result_min.name, 
                           'email': result_min.email, 
                           'department': result_min.department, 
-                          'salary': result_min.salary}
+                          'salary': result_min.salary,
+                          'birth_date': result_min.birth_date}
 
             employee_2 = {'id': result_max.id,
                           'name': result_max.name, 
                           'email': result_max.email, 
                           'department': result_max.department, 
-                          'salary': result_max.salary}          
+                          'salary': result_max.salary,
+                          'birth_date': result_max.birth_date}          
 
             data = {'average': avg_salary,
                     'lowest': employee_1,
                     'highest': employee_2}                            
                                          
             return data
-  
+
+#Resource referring to the Age Reports
 class Reports_age(Resource):
     #Resource Fields -> Age report
     reports_age_fields = {
@@ -187,7 +196,8 @@ class Reports_age(Resource):
                                 'salary': fields.Float},
                             'average': fields.Float,
                          }
-
+                         
+    #Implementation of the GET method 
     @auth.login_required
     @marshal_with(reports_age_fields)
     def get(self):            
@@ -197,8 +207,8 @@ class Reports_age(Resource):
 
 #Routes setup
 api.add_resource(Employees, "/employees/<int:employee_id>", "/employees/")
-api.add_resource(Reports_age, "/reports/employees/age/" )
 api.add_resource(Reports_salary, "/reports/employees/salary/" )
+api.add_resource(Reports_age, "/reports/employees/age/" )
 
 if __name__ == "__main__":
     app.run(debug=True)
